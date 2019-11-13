276c0df2c52e1d0f537e90aaf1425d2a1abf7ee4
Hayes Neuman <neurosie@gmail.com>
2019-11-13 12:28:09 -0500

    Make OntoAV subclass of MalmoAgent

--- agent/agent.py
+++ agent/agent.py
@@ -1,3 +1,8 @@
+from sys import path
+from os.path import dirname as dir
+
+path.append(dir(path[0]))
+__package__ = "worldtest"
 from ontocraft.agent import MalmoAgent
 from ontocraft.effectors.move import MoveAMR
 from ontocraft.effectors.speech import SpeechTMR
@@ -6,29 +11,21 @@ from ontocraft.utils.MalmoUtils import OntoCraftAgentHost, bootstrap
 import time
 
 
-class OntoAV:
-
-    def __init__(self, agent: MalmoAgent = None, host: OntoCraftAgentHost = None, effectors: list = None):
-        if host is None:
-            self.host = bootstrap(("resources", "world.xml"))
-            self.host.sendCommand("hotbar.9 1")
-            time.sleep(0.5)
-
-        if agent is None:
-            self.agent = MalmoAgent.build(self.host)
-
-        print(self.agent.anchor["HAS-NAME"].singleton())
-
-        self.agent.observe(join=True)
+class OntoAV(MalmoAgent):
+    @classmethod
+    def build(cls, host: OntoCraftAgentHost) -> 'OntoAV':
+        agent = super().build(host)
+        agent = OntoAV(agent.anchor)
+        return agent
 
         # TODO:
         #   [] - Do something with effectors
         #   [] - Build utilities for building movement AMRs paths automatically
         #   [] - Build utilities for moving, observing, and generating output (speech and movement)
 
-    def speak(self, string: str):
+    def speaksentence(self, string: str):
         tmr = SpeechTMR.build(string)
-        self.agent.speak(tmr, join=True)
+        self.speak(tmr, join=True)
 
     @staticmethod
     def move_forward(amr: MoveAMR = None, distance: int = 1):
@@ -74,26 +71,24 @@ class OntoAV:
         if mvmt[0] == "ccw":
             amrs.append(self.turn_ccw(times=mvmt[1]))
         for amr in amrs:
-            self.agent.move(amr, join)
+            self.move(amr, join)
             self.observe(join)
 
-    def move(self, input: str):
+    def movepath(self, input: str):
         input = [item.strip() for item in input.split(',')]
         for mvmt in input:
             mvmt = list(map(lambda x: int(x) if x.isdigit() else x, mvmt.split("x")))
             self._move(mvmt)
 
-    def observe(self, join=True):
-        self.agent.observe(join)
-
-    def environment(self):
-        return self.agent.environment()
-
 
 if __name__ == '__main__':
-    agent = OntoAV()
+    host = bootstrap(("resources", "world.xml"))
+    host.sendCommand("hotbar.9 1")
+    time.sleep(0.5)
+
+    agent = OntoAV.build(host)
 
     planned_path = "cwx1, fx1, ccwx1, fx5"
-    agent.move(planned_path)
+    agent.movepath(planned_path)
 
-    print(agent.environment().relative_block(agent.agent, 0, 1, 1).type())
+    print(agent.environment().relative_block(agent, 0, 1, 1).type())
