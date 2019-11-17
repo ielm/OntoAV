1cf3f0cc53687f3ef5d5e7bf13ccd6b77cf6263d
Hayes Neuman <neurosie@gmail.com>
2019-11-16 20:27:16 -0500

    Route planning with obstacles

--- worldtest/run.py
+++ worldtest/run.py
@@ -2,22 +2,25 @@ from ontocraft.agent import MalmoAgent
 from ontocraft.effectors.move import MoveAMR
 from ontocraft.effectors.speech import SpeechTMR
 from ontocraft.utils.MalmoUtils import OntoCraftAgentHost, bootstrap
+from pkgutil import get_data
 from clagent.agent import CLAgent
 
 import time
 
 
 if __name__ == '__main__':
-	host = bootstrap(("resources", "world.xml"))
+	map_file = ("worldtest.resources", "map.txt")
+
+	host = bootstrap(("worldtest.resources", "world.xml"))
 	host.sendCommand("hotbar.9 1")
 	time.sleep(0.5)
 
 	# Build a new agent
-	agent = CLAgent.build(host)
-	print(agent.anchor["HAS-NAME"].singleton())
+	agent = CLAgent.build(host, ("worldtest.resources", "map.txt"))
+	agent.navigate_to('G')
 
-	agent.movepath("fx18, cwx1, fx12, cwx1")
+	# agent.movepath("fx18, cwx1, fx12, cwx1")
 
 	# Say something
-	agent.speaksentence("Hello world.")
+	# agent.speaksentence("Hello world.")
 
