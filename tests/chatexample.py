97e92621b399e73b22c8c7e2b1a2826d36741f80
ielm <leoni@rpi.edu>
2019-11-18 21:48:47 -0500

    add demo for command handling

--- tests/chatexample.py
+++ tests/chatexample.py
@@ -7,7 +7,7 @@ from ontocraft.utils.MalmoUtils import bootstrap_specific
 from ontograph.Frame import Frame
 
 from clagent.agent import CLAgent
-from clagent.handle import ChatHandleExecutable
+from clagent.handle import CommandHandleExecutable
 
 import time
 
@@ -20,40 +20,35 @@ if __name__ == '__main__':
     def bootstrap_user():
         agent_host = bootstrap_specific(("resources", "25world.xml"), clients, 0)
 
-        user = CLAgent.build(agent_host)
+        agent = CLAgent.build(agent_host)
 
         action = False
-        while user.host().getWorldState().is_mission_running:
+        while agent.host().getWorldState().is_mission_running:
             time.sleep(2)
 
             if not action:
-                tmr = SpeechTMR.build("Hello world.")
-                user.speak(tmr, join=True)
+                tmr = SpeechTMR.build("My name is Ozymandias, king of agents: Look on my works, ye Mighty, and despair!")
+                agent.speak(tmr, join=True)
                 action = True
 
+    # The agent, after the mission loads, will observe every 1 second.  When he finds a chat signal,
+    # it will be analyzed and execute a chat handler which checks for movement commands. On
+    # a movement command, the agent will execute the movement command.
     def bootstrap_agent():
         agent_host = bootstrap_specific(("resources", "25world.xml"), clients, 1)
 
         agent = CLAgent.build(agent_host)
 
-        # Jake needs to know who Sasha is, so the input signal can be attributed to the speaker correctly.
+        # The agent needs to know who the User is, so the input signal can be attributed to the speaker correctly.
         user = Frame("@ENV.AGENT.?").add_parent("@ONT.AGENT")
-        user["HAS-NAME"] = "USER"
+        user["HAS-NAME"] = "User"
 
-        # Disable all of the other signal observations for optimization of this example.
-        from ontocraft.observers.position import PositionSignal
-        from ontocraft.observers.vision import SupervisionSignal
-        agent.disable_observer(PositionSignal)
-        agent.disable_observer(SupervisionSignal)
-
-        # Make sure Jake knows how to respond to an analyzed speech act.
-        agent.add_response(Frame("@ONT.SPEECH-ACT"), ChatHandleExecutable)
+        # Make sure the agent knows how to respond to an analyzed speech act.
+        agent.add_response(Frame("@ONT.SPEECH-ACT"), CommandHandleExecutable)
 
         while agent.host().getWorldState().is_mission_running:
-            time.sleep(2)
-
+            time.sleep(1)
             agent.observe(join=True)
-
 
     # Make a process for bootstrapping and running each agent; they do not need to be joined or timed otherwise.
     from multiprocessing import Process
