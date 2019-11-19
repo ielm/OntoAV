94c3e8b08b0e5ac239142c057781836a53e687b8
ielm <leoni@rpi.edu>
2019-11-18 21:49:04 -0500

    add command handling

--- clagent/handle.py
+++ clagent/handle.py
@@ -1,16 +1,72 @@
 from ontoagent.agent import Agent
 from ontoagent.engine.executable import HandleExecutable
 from ontoagent.engine.signal import Signal, XMR
+from ontograph.Frame import Frame
 from ontocraft.effectors.speech import SpeechTMR
 from ontocraft.observers.chat import ChatTMR
-# from clagent.agent import CLAgent
+from clagent.agent import CLAgent
 
 
-class ChatHandleExecutable(HandleExecutable):
+class CommandHandleExecutable(HandleExecutable):
 
-    def validate(self, agent: Agent, signal: Signal) -> bool:
-        return True
+    def validate(self, agent: CLAgent, signal: Signal) -> bool:
+        return signal.root() ^ Frame("@ONT.SPEECH-ACT") and signal.root()["THEME"].singleton() ^ Frame("@ONT.RAW-TEXT") and signal.agent().id == "@ENV.AGENT.1"
 
-    def run(self, agent: Agent, signal: ChatTMR):
-        tmr = SpeechTMR.build("I heard %s say '%s'." % (signal.agent().id, signal.raw_text()))
+    def run(self, agent: CLAgent, signal: ChatTMR):
+        text = signal.raw_text()
+        path = None
+        if self.is_pretentious(text):
+            tmr = SpeechTMR.build("Nothing beside remains. Round the decay Of that colossal wreck, boundless and bare The lone and level sands stretch far away.")
+        elif self.is_command(text):
+            path, tmr = self.parse_command(text)
+        else:
+            tmr = SpeechTMR.build("I heard %s say '%s'." % (signal.agent().id, text))
         agent.speak(tmr, join=True)
+        if path is not None:
+            agent.movepath(path)
+
+    @staticmethod
+    def is_pretentious(signal: str) -> bool:
+        if signal == "My name is Ozymandias, king of agents: Look on my works, ye Mighty, and despair!":
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def is_command(signal: str) -> bool:
+        if True in list(map(lambda c: c in signal.lower(), ["move", "walk", "turn", "go"])):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def parse_command(signal: str):
+        command = signal.lower()
+
+        def _get_digits(text):
+            return ''.join(filter(lambda x: x.isdigit(), text))
+
+        if "move" in command:
+            if "forward" in command:
+                digit = _get_digits(command)
+                digit = "1" if digit == "" else digit
+                path = "fx" + digit
+                return path, SpeechTMR.build("Moving forward %s step(s)" % digit)
+            if "backward" in command:
+                digit = _get_digits(command)
+                digit = "1" if digit == "" else digit
+                path = "bx" + digit
+                return path, SpeechTMR.build("Moving backward %s step(s)" % digit)
+
+        if "turn" in command:
+            if True in list(map(lambda c: c in command, ["right", "clockwise", "cw"])):
+                digit = _get_digits(command)
+
+                digit = "1" if digit == "" else digit
+                path = "cwx" + digit
+                return path, SpeechTMR.build("Turning clockwise %s step(s)" % digit)
+            if True in list(map(lambda c: c in command, ["left", "counterclockwise", "ccw"])):
+                digit = _get_digits(command)
+                digit = "1" if digit == "" else digit
+                path = "ccwx" + digit
+                return path, SpeechTMR.build("Turning counter-clockwise %s step(s)" % digit)
